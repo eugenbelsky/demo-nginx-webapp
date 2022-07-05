0fc7e11b9fbf46cf90299a9aef84714457535d72
Eugen Belskyi <eugen.belskyi@dynatrace.com>
2022-07-05 10:21:03 +0200

    add crds

--- terraform/cert-manager.tf
+++ terraform/cert-manager.tf
@@ -1,3 +1,17 @@
+data "kubectl_filename_list" "cm-crds" {
+  pattern = "./crds/cert-manager.crds.yaml"
+}
+
+
+resource "kubectl_manifest" "cm-crds" {
+  count              = length(data.kubectl_filename_list.cm-crds.matches)
+  override_namespace = local.cm_namespace
+  yaml_body          = file(element(data.kubectl_filename_list.cm-crds.matches, count.index))
+  depends_on = [
+    google_container_cluster.primary
+  ]
+}
+
 resource "helm_release" "cert-manager" {
   name             = local.cm_release
   repository       = local.cm_repo
@@ -8,24 +22,24 @@ resource "helm_release" "cert-manager" {
   atomic           = true
 
   depends_on = [
-    google_container_cluster.primary
+    kubectl_manifest.cm-crds
   ]
 }
 
 
-data "kubectl_filename_list" "cm-cluster-issuer" {
-  pattern = "./crds/cluster-issuer.yaml"
-}
+# data "kubectl_filename_list" "cm-cluster-issuer" {
+#   pattern = "./crds/cluster-issuer.yaml"
+# }
 
 
-resource "kubectl_manifest" "cm-cluster-issuer" {
-  count              = length(data.kubectl_filename_list.cm-cluster-issuer.matches)
-  override_namespace = local.cm_namespace
-  yaml_body          = file(element(data.kubectl_filename_list.cm-cluster-issuer.matches, count.index))
-  depends_on = [
-    helm_release.cert-manager
-  ]
-}
+# resource "kubectl_manifest" "cm-cluster-issuer" {
+#   count              = length(data.kubectl_filename_list.cm-cluster-issuer.matches)
+#   override_namespace = local.cm_namespace
+#   yaml_body          = file(element(data.kubectl_filename_list.cm-cluster-issuer.matches, count.index))
+#   depends_on = [
+#     helm_release.cert-manager
+#   ]
+# }
 
 
 
